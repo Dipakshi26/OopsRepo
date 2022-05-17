e91c13cb3f634379cf299c4d8c4d5841f8cc7a85
Shatakshi Saxena <shatakshi16saxena@gmail.com>
2022-05-17 18:44:03 +0530

    updated files

---
+++ OOps Concepts/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOps_Concepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Users user = new Users("Suresh Desari", 30);
+            user.GetUserDetails();
+            Console.ReadLine();
+        }
+    }
+}
